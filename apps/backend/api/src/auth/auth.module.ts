1af596ad6a0c87f9a630fd3655e7d5260074bff6
codelande <corentin.delande.1@gmail.com>
2022-12-07 14:27:58 +0100

    fix bug auth

--- apps/backend/api/src/auth/auth.module.ts
+++ apps/backend/api/src/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  providers: [AuthService, UsersService],
+  imports: [UsersModule, TypeOrmModule.forFeature([User])],
 })
 export class AuthModule {}
