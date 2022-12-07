8dad0ec67d72f27a3f1c32273b038bd18c1bc9e3
codelande <corentin.delande.1@gmail.com>
2022-12-07 12:03:10 +0100

    create the user entity

---
+++ apps/backend/api/src/config/database.config.ts
@@ -0,0 +1,14 @@
+import { DataSourceOptions } from 'typeorm';
+import 'dotenv/config';
+
+export const databaseConfig: DataSourceOptions = {
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT || '3000'),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DB,
+  entities: [],
+  synchronize: false,
+  migrations: [__dirname + '/../migrations/*.ts'],
+};
