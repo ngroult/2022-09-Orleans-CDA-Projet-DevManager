55099a3372daf0e07db95ee45af5441135f8b427
ngroult <nolwenn.groult@gmail.com>
2022-12-07 11:37:33 +0100

    Remove Box and Heading in App

--- apps/frontend/web/src/App.tsx
+++ apps/frontend/web/src/App.tsx
@@ -29,30 +29,6 @@ function App() {
   }, []);
 
   return <div className="App"><div>{`${someData.someProperty}`}</div>
-    <Heading>DevManager</Heading>
-    <Box bg="turquoise.200">Turquoise 200</Box>
-    <Box bg="turquoise.300">Turquoise 300</Box>
-    <Box bg="turquoise.500">Turquoise 500</Box>
-    <Box bg="turquoise.900">Turquoise 900</Box>
-
-    <Box bg="blue.200">Blue 200</Box>
-    <Box bg="blue.500">Blue 500</Box>
-    <Box bg="blue.900">Blue 900</Box>
-
-    <Box bg="purple.200">Purple 200</Box>
-    <Box bg="purple.300">Purple 300</Box>
-    <Box bg="purple.500">Purple 500</Box>
-    <Box bg="purple.900">Purple 900</Box>
-
-    <Box bg="pink.200">Pink 200</Box>
-    <Box bg="pink.500">Pink 500</Box>
-    <Box bg="pink.900">Pink 900</Box>
-
-    <Box bg="gold.200">Gold 200</Box>
-    <Box bg="gold.300">Gold 300</Box>
-    <Box bg="gold.500">Gold 500</Box>
-    <Box bg="gold.900">Gold 900</Box>
-
   </div>;
 }
 
