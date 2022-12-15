b6f05e890b9a84be46c8ad8ef338ad2ef09aefb1
codelande <corentin.delande.1@gmail.com>
2022-12-15 12:36:58 +0100

    finish the changes requested correction

--- apps/backend/api/src/auth/auth.service.ts
+++ apps/backend/api/src/auth/auth.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
-import * as crypto from 'crypto';
 import { User } from '../entities/user.entity';
+import * as argon2 from 'argon2';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
-  public async register(user: User): Promise<any> {
-    user.password = this.hash(user.password);
+  public async register(user: User): Promise<CreateUserDto> {
+    user.password = await this.hash(user.password);
 
     return this.usersService.create(user);
   }
 
-  private hash(password: string): string {
-    return crypto.createHmac('sha256', password).digest('hex');
+  private hash(password: string) {
+    const hash = argon2.hash(password);
+    return hash;
   }
 }
