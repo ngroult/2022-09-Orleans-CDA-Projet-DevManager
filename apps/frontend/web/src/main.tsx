3c997277796c394906ab9d5fd5225a45f6182114
ngroult <nolwenn.groult@gmail.com>
2022-12-15 09:41:13 +0100

    fix (frontend web) : remove 'index' in import theme from './theme' in main.tsx

--- apps/frontend/web/src/main.tsx
+++ apps/frontend/web/src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import '@fontsource/chakra-petch/400.css';
 import '@fontsource/orbitron/700.css';
-import theme from './theme/index';
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ChakraProvider theme={theme}>
