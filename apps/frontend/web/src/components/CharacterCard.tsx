af1fbe9fc897c59850a1ceacc68b0ea9e4242833
codelande <corentin.delande.1@gmail.com>
2022-12-15 16:37:34 +0100

    card components for charachter

--- apps/frontend/web/src/components/CharacterCard.tsx
+++ apps/frontend/web/src/components/CharacterCard.tsx
@@ -4,11 +4,10 @@ import {
   Button,
   Card,
   CardBody,
-  CardFooter,
+  Center,
+  Flex,
   Heading,
-  HStack,
   Image,
-  Stack,
   Text,
 } from '@chakra-ui/react';
 
@@ -18,30 +17,63 @@ function CharacterCard() {
       direction={{ base: 'column', sm: 'row' }}
       overflow="hidden"
       variant="outline"
+      bg="blue.100"
     >
       <Box borderRightRadius="2xl" w="100px" h="100px" bg="blue" shadow="2xl">
-
+        <Image
+          boxSize="80%"
+          m="auto"
+          mt="2.5"
+          src="lead_dev.png"
+          alt="lead developer"
+        />
       </Box>
 
-      <HStack>
-        <CardBody>
-          <Heading size="md">The perfect latte</Heading>
+      <CardBody>
+        <Flex alignItems="center" w="full" justifyContent="space-between">
+          <Box>
+            <Heading size="md" mb="1">
+              {'Lead Developer'}
+            </Heading>
+            <Badge borderRadius="full" px="2" colorScheme="teal">
+              <Flex align="center">
+                <Image src="/coffee.png" alt="coffee" boxSize="30px" p="1" />
+                <Text>{'Coffee'}</Text>
+              </Flex>
+            </Badge>
+            <Badge borderRadius="full" px="2" colorScheme="teal">
+              <Flex align="center">
+                <Image src="/coffee.png" alt="coffee" boxSize="30px" p="1" />
+                <Text>{'Coffee'}</Text>
+              </Flex>
+            </Badge>
+            <Badge borderRadius="full" px="2" colorScheme="red">
+              <Flex align="center">
+                <Image src="/coffee.png" alt="coffee" boxSize="30px" p="1" />
+                <Text>{'Coffee'}</Text>
+              </Flex>
+            </Badge>
+          </Box>
+          <Box>
+            <Badge
+              fontSize="xl"
+              borderRadius="full"
+              px="2"
+              colorScheme="yellow"
+              marginEnd="5"
+            >
+              <Flex align="center">
+                <Image src="/dollar.png" alt="dollar" boxSize="30px" p="1" />
+                {'100$'}
+              </Flex>
+            </Badge>
 
-          <Badge borderRadius="full" px="2" colorScheme="teal">
-            ressource 1
-          </Badge>
-          <Badge borderRadius="full" px="2" colorScheme="teal">
-            ressource 2
-          </Badge>
-        </CardBody>
-        <Badge borderRadius="full" px="2" colorScheme="yellow">
-          100$
-        </Badge>
-
-        <Button variant="solid" colorScheme="blue" shadow="2xl">
-          + 1
-        </Button>
-      </HStack>
+            <Button variant="unstyled" bg="blue" boxShadow="2xl" size="lg">
+              {'+ 1'}
+            </Button>
+          </Box>
+        </Flex>
+      </CardBody>
     </Card>
   );
 }
