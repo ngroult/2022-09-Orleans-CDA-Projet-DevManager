eb4c259a0d9ae00e96cb558c2a5f719d3a8f087d
Paul Chatelain-Fernandes <paulchatelainfernandes@gmail.com>
2022-12-16 17:06:49 +0100

    fix(backend/api): added option to id column of game entity

--- apps/backend/api/src/games/entities/game.entity.ts
+++ apps/backend/api/src/games/entities/game.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Game {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({ unsigned: true })
   id: number;
 
   @Column('int', { unsigned: true })
