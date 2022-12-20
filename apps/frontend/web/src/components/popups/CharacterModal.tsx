2445475b568011def7355ccd38ba038539e01756
codelande <corentin.delande.1@gmail.com>
2022-12-20 15:11:49 +0100

    feat:start

---
+++ apps/frontend/web/src/components/popups/CharacterModal.tsx
@@ -0,0 +1,45 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
+
+function CharacterModal() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>
+                lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                lorrem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis maxime fugiat pariatur aliquid, ut ratione neque? Quam laudantium ipsa velit, placeat reiciendis rerum, totam quo dicta expedita itaque ea eius?
+            </Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default CharacterModal;
