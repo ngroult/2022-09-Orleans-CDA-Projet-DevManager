367f49aef1bcef7c5480301dc652a380d9d91607
codelande <corentin.delande.1@gmail.com>
2022-12-20 16:36:22 +0100

    fix:paul

--- apps/frontend/web/src/main.tsx
+++ apps/frontend/web/src/main.tsx
@@ -4,9 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import '@fontsource/chakra-petch/400.css';
 import '@fontsource/orbitron/700.css';
-import CharacterCard from './components/CharacterCard';
 import theme from './theme';
-import Login from './pages/Login';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
