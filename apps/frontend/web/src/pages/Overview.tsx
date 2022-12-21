a3287e59a6036a15bc2502315ee2269fcd84d225
Paul Chatelain-Fernandes <paulchatelainfernandes@gmail.com>
2022-12-21 10:21:30 +0100

    feat(frontend/web): add button to buy first room

--- apps/frontend/web/src/pages/Overview.tsx
+++ apps/frontend/web/src/pages/Overview.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Box, Image, Text } from '@chakra-ui/react';
+import { Button, Box, Image, Text } from '@chakra-ui/react';
 
 const Overview = () => {
+  const handleBuyFirstRoom = () => {};
+
   return (
-    <Box>
+    <Box display="flex" flexDir="column" alignItems="center">
       <Text fontSize="xl">{'Overview'}</Text>
       <Image src="/overview.jpg" />
+      <Button colorScheme="teal" size="lg" onClick={handleBuyFirstRoom}>
+        {'Buy an OpenSpace'}
+        <br />
+        {'$1000'}
+      </Button>
     </Box>
   );
 };
