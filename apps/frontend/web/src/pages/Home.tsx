8edf59a1f30ab16f4344c88270de4c584235cd8f
SkeidoK <98970084+SkeidoK@users.noreply.github.com>
2023-01-05 17:12:39 +0100

    feat: create home page with route register

--- apps/frontend/web/src/pages/Home.tsx
+++ apps/frontend/web/src/pages/Home.tsx
@@ -1,5 +1,46 @@
+import { Box, Button, Flex, Image, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-  return <div>{'Home'}</div>;
+  return (
+    <Box>
+      <Image
+        src="/overview.jpg"
+        h="100vh"
+        objectFit="cover"
+        alt="Overview"
+        zIndex={-1}
+        position="absolute"
+      ></Image>
+      <Flex justify="center" m="20">
+        <Box
+          h="25vh"
+          bg="#797AA6"
+          opacity="90%"
+          border="1px"
+          borderRadius="5px"
+        >
+          <Text color="white" m="5px">
+            {
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at nisl leo. Hfjdshfgj, hfdpcqssqiofjfkfv'
+            }
+          </Text>
+        </Box>
+      </Flex>
+      <Flex justify="center" mb="7">
+        <Button bg="#42B7B4" color="white">
+          {'Sign Up'}
+        </Button>
+      </Flex>
+      <Flex justify="center">
+        <Link to="/register">
+        <Button bg="#797AA6" color="white" >
+          {'Create your account'}
+        </Button>
+        </Link>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Home;
