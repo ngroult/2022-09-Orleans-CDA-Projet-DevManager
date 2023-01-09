a7571280e000a51e17295fe4c01577cfbe43e696
codelande <co.delande@yahoo.fr>
2023-01-09 11:04:55 +0100

    fix/box bottom

--- apps/frontend/web/src/components/popups/CharacterModal.tsx
+++ apps/frontend/web/src/components/popups/CharacterModal.tsx
@@ -10,7 +10,6 @@ import {
   ModalBody,
   ModalCloseButton,
   ModalContent,
-  Spacer,
   Text,
   useDisclosure,
 } from '@chakra-ui/react';
@@ -25,94 +24,96 @@ function CharacterModal() {
         <ModalContent bg={'purple.200'}>
           <ModalCloseButton />
           <ModalBody pt={'10'} px={'0'}>
-            <Center>
-              <Image
-                boxSize="35%"
-                m="auto"
-                mt="2.5"
-                src="/lead_dev.png"
-                alt="lead developer"
-              />
-            </Center>
-            <Center>
-              <Heading size="md" py={'10'}>
-                {'Lead Developer'}
-              </Heading>
-            </Center>
-            <Text textAlign={'center'}>
-              {
-                'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis maxime fugiat pariatur aliquid, ut ratione neque? Quamlaudantium ipsa velit, placeat reiciendis rerum, totam quo dictaexpedita itaque ea eius?'
-              }
-            </Text>
-            <Center>
-              <Flex py={'10'}>
-                <Text mr={'3'} fontWeight={'extrabold'}>
-                  {'Current price :'}
-                </Text>
-                <Badge
-                  fontSize="xl"
-                  borderRadius="full"
-                  px="2"
-                  colorScheme="yellow"
-                  marginEnd="5"
-                >
-                  <Flex align="center">
-                    <Image
-                      src="/dollar.png"
-                      alt="dollar"
-                      boxSize="30px"
-                      p="1"
-                    />
-                    {'$100'}
-                  </Flex>
-                </Badge>
-                <Text fontWeight={'extrabold'}>{'/ unit'}</Text>
-              </Flex>
-            </Center>
-            <Center>
-              <Text fontWeight={'extrabold'}>
-                {'Ressource produced and used:'}
+            <Box h={'calc(100vh-7rem)'}>
+              <Center>
+                <Image
+                  boxSize="35%"
+                  m="auto"
+                  mt="2.5"
+                  src="/lead_dev.png"
+                  alt="lead developer"
+                />
+              </Center>
+              <Center>
+                <Heading size="md" py={'10'}>
+                  {'Lead Developer'}
+                </Heading>
+              </Center>
+              <Text textAlign={'center'}>
+                {
+                  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis maxime fugiat pariatur aliquid, ut ratione neque? Quamlaudantium ipsa velit, placeat reiciendis rerum, totam quo dictaexpedita itaque ea eius?'
+                }
               </Text>
-            </Center>
-            <Center>
-              <Box pt={'8'}>
-                <Badge borderRadius="full" px="2" colorScheme="teal">
-                  <Flex align="center">
-                    <Image
-                      src="/coffee.png"
-                      alt="coffee"
-                      boxSize="30px"
-                      p="1"
-                    />
-                    <Text>{'Coffee'}</Text>
-                  </Flex>
-                </Badge>
-                <Badge borderRadius="full" px="2" colorScheme="teal">
-                  <Flex align="center">
-                    <Image
-                      src="/coffee.png"
-                      alt="coffee"
-                      boxSize="30px"
-                      p="1"
-                    />
-                    <Text>{'Coffee'}</Text>
-                  </Flex>
-                </Badge>
-                <Badge borderRadius="full" px="2" colorScheme="red">
-                  <Flex align="center">
-                    <Image
-                      src="/coffee.png"
-                      alt="coffee"
-                      boxSize="30px"
-                      p="1"
-                    />
-                    <Text>{'Coffee'}</Text>
-                  </Flex>
-                </Badge>
-              </Box>
-            </Center>
-            <Box bottom={'1'}>
-              <Box h={'4rem'} bg={'blue'} w={'100%'}></Box>
+              <Center>
+                <Flex py={'10'}>
+                  <Text mr={'3'} fontWeight={'extrabold'}>
+                    {'Current price :'}
+                  </Text>
+                  <Badge
+                    fontSize="xl"
+                    borderRadius="full"
+                    px="2"
+                    colorScheme="yellow"
+                    marginEnd="5"
+                  >
+                    <Flex align="center">
+                      <Image
+                        src="/dollar.png"
+                        alt="dollar"
+                        boxSize="30px"
+                        p="1"
+                      />
+                      {'$100'}
+                    </Flex>
+                  </Badge>
+                  <Text fontWeight={'extrabold'}>{'/ unit'}</Text>
+                </Flex>
+              </Center>
+              <Center>
+                <Text fontWeight={'extrabold'}>
+                  {'Ressource produced and used:'}
+                </Text>
+              </Center>
+              <Center>
+                <Box pt={'8'}>
+                  <Badge borderRadius="full" px="2" colorScheme="teal">
+                    <Flex align="center">
+                      <Image
+                        src="/coffee.png"
+                        alt="coffee"
+                        boxSize="30px"
+                        p="1"
+                      />
+                      <Text>{'Coffee'}</Text>
+                    </Flex>
+                  </Badge>
+                  <Badge borderRadius="full" px="2" colorScheme="teal">
+                    <Flex align="center">
+                      <Image
+                        src="/coffee.png"
+                        alt="coffee"
+                        boxSize="30px"
+                        p="1"
+                      />
+                      <Text>{'Coffee'}</Text>
+                    </Flex>
+                  </Badge>
+                  <Badge borderRadius="full" px="2" colorScheme="red">
+                    <Flex align="center">
+                      <Image
+                        src="/coffee.png"
+                        alt="coffee"
+                        boxSize="30px"
+                        p="1"
+                      />
+                      <Text>{'Coffee'}</Text>
+                    </Flex>
+                  </Badge>
+                </Box>
+              </Center>
+            </Box>
+            <Box pos={'absolute'} bottom={'0'} w={'100%'}>
+              <Box h={'4rem'} bg={'blue'}></Box>
               <Box h={'2rem'} bg={'green'}></Box>
               <Box h={'1rem'} bg={'pink'}></Box>
             </Box>
