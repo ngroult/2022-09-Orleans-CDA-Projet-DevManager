3d52d549fc2902f037ea9016c54996b59f5703d4
codelande <co.delande@yahoo.fr>
2023-01-09 12:07:55 +0100

    fix: main

--- apps/frontend/web/src/main.tsx
+++ apps/frontend/web/src/main.tsx
@@ -4,16 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import '@fontsource/chakra-petch/400.css';
 import '@fontsource/orbitron/700.css';
-import CharacterCard from './components/CharacterCard';
 import theme from './theme';
 import App from './App';
-import EventModal from './components/popups/EventModal';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
       <BrowserRouter>
-        <EventModal />
+        <App />
       </BrowserRouter>
     </ChakraProvider>
   </StrictMode>
