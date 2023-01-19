c181c9469c028bf9ce683201f4914a37880f2b67
ngroult <nolwenn.groult@gmail.com>
2023-01-19 17:10:50 +0100

    feat(frontend web): completed DTO CreateResource

--- apps/backend/api/src/app/resources/dto/create-resource.dto.ts
+++ apps/backend/api/src/app/resources/dto/create-resource.dto.ts
@@ -1,1 +1,21 @@
-export class CreateResourceDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateResourceDto {
+  @IsNotEmpty()
+  id: number;
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  description: string;
+
+  @IsNotEmpty()
+  image: string;
+
+  @IsNotEmpty()
+  price: number;
+
+  @IsNotEmpty()
+  color: string;
+}
