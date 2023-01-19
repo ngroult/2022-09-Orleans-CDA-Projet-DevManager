f89b0953d46e093c7761c3cd97415cb2be5972b7
SkeidoK <98970084+SkeidoK@users.noreply.github.com>
2023-01-19 11:23:22 +0100

    feat: create page 404

--- apps/frontend/web/src/pages/Page404.tsx
+++ apps/frontend/web/src/pages/Page404.tsx
@@ -1,5 +1,24 @@
+import { Box, Center, Image, Text } from '@chakra-ui/react';
+
 const Page404 = () => {
-  return <div>{'Page404'}</div>;
+  return (
+    <Box>
+      <Center>
+        <Image
+          src="/404.jpg"
+          alt="Dev dort"
+          w={{ base: '60%', sm: '60%', md: '80%', lg: '60%' }}
+          borderRadius="10"
+          mt="10"
+        />
+      </Center>
+      <Center>
+        <Text fontSize="3xl" mt="20" p="5">
+          {"Oups, c'est la salle 404! C'est rien c'est juste le stagaire!"}
+        </Text>
+      </Center>
+    </Box>
+  );
 };
 
 export default Page404;
