eb1e941f4f10e27585955a1e6d91dc1685841d52
ngroult <nolwenn.groult@gmail.com>
2023-01-24 11:40:42 +0100

    fix(frontend web): resolved conflicts

--- apps/frontend/web/src/App.tsx
+++ apps/frontend/web/src/App.tsx
@@ -49,54 +49,11 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
         <Route path="/new-game" element={<NewGame />} />
         <Route path="/game/overview" element={<Overview />} />
         <Route path="/game/:label" element={<RoomPage />} />
         <Route path="/game-settings" element={<GameSettings />} />
         <Route path="/account-settings" element={<AccountSettings />} />
-=======
-        <Route
-          path="/new-game"
-          element={
-            <Protected>
-              <NewGame />
-            </Protected>
-          }
-        />
-        <Route
-          path="/game/overview"
-          element={
-            <Protected>
-              <Overview />
-            </Protected>
-          }
-        />
-        <Route
-          path="/game/:room"
-          element={
-            <Protected>
-              <Room />
-            </Protected>
-          }
-        />
-        <Route
-          path="/game-settings"
-          element={
-            <Protected>
-              <GameSettings />
-            </Protected>
-          }
-        />
-        <Route
-          path="/account-settings"
-          element={
-            <Protected>
-              <AccountSettings />
-            </Protected>
-          }
-        />
->>>>>>> 03da2346d9eaae974d547720d72c8b8f1cfeca6e
         <Route path="/about" element={<About />} />
         <Route path="/assistance" element={<Assistance />} />
         <Route path="*" element={<Page404 />} />
