116c9793049943cad6094d79edba44430515f208
Paul Chatelain-Fernandes <paulchatelainfernandes@gmail.com>
2023-01-26 12:49:02 +0100

    feat(frontend/web): handle user errors and display them in login page

--- apps/frontend/web/src/pages/Login.tsx
+++ apps/frontend/web/src/pages/Login.tsx
@@ -14,23 +14,29 @@ import {
 } from '@chakra-ui/react';
 import { useContext, useState } from 'react';
 import { useLocation, useNavigate, Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import AuthContext from '../contexts/AuthContext';
 
 function Login() {
-  const [show, setShow] = useState(false);
-  const handleClick = () => setShow(!show);
-  const navigate = useNavigate();
-
+  const [isVisiblePassword, setIsVisiblePassword] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isIncorrectLogs, setIsIncorrectLogs] = useState(false);
+  const [isServerError, setIsServerError] = useState(false);
   const { setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
   const location = useLocation();
 
-  const [error, setError] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const isError = error !== '';
+  const submitLogin = async () => {
+    isIncorrectLogs ? setIsIncorrectLogs(false) : null;
+    isServerError ? setIsServerError(false) : null;
 
-  const handleSubmit = async () => {
     try {
       const loginResponse = await fetch('/api/auth/login', {
         method: 'POST',
@@ -43,7 +49,7 @@ function Login() {
       const jsonResponse = await loginResponse.json();
 
       if (jsonResponse.status === 'KO') {
-        setError('Username and / or password incorrect');
+        setIsIncorrectLogs(true);
       }
       if (jsonResponse.status === 'OK') {
         setUser(jsonResponse.data);
@@ -54,7 +60,7 @@ function Login() {
         }
       }
     } catch (err) {
-      console.error(err);
+      setIsServerError(true);
     }
   };
 
@@ -84,10 +90,8 @@ function Login() {
         alignItems="center"
         justifyContent="center"
         p="2rem 2rem 0"
-        isInvalid={isError}
+        isInvalid={errors.username || errors.password ? true : false}
       >
-        {isError && <FormErrorMessage>{error}</FormErrorMessage>}
-
         <FormLabel textAlign="left" m="1rem 0 0" w="100%" maxW="400px">
           {'Username'}
         </FormLabel>
@@ -97,11 +101,15 @@ function Login() {
           bgColor="#fff"
           _placeholder={{ opacity: 0.3 }}
           type="text"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
+          {...register('username', {
+            required: 'Your username is required',
+            value: username,
+            onChange: (e) => setUsername(e.target.value),
+          })}
         />
+        <FormErrorMessage>
+          {`${errors.username && errors.username.message}`}
+        </FormErrorMessage>
 
         <FormLabel textAlign="left" m="1rem 0 0" w="100%" maxW="400px">
           {'Password'}
@@ -112,15 +120,21 @@ function Login() {
             placeholder="Enter password..."
             bgColor="#fff"
             _placeholder={{ opacity: 0.3 }}
-            type={show ? 'text' : 'password'}
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
+            type={isVisiblePassword ? 'text' : 'password'}
+            {...register('password', {
+              required: 'Your password is required',
+              value: password,
+              onChange: (e) => setPassword(e.target.value),
+            })}
           />
           <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick} me="1">
-              {show ? (
+            <Button
+              h="1.75rem"
+              size="sm"
+              onClick={() => setIsVisiblePassword(!isVisiblePassword)}
+              me="1"
+            >
+              {isVisiblePassword ? (
                 <div>
                   {'Hide'} <ViewOffIcon ms="0.5" />
                 </div>
@@ -132,6 +146,9 @@ function Login() {
             </Button>
           </InputRightElement>
         </InputGroup>
+        <FormErrorMessage>
+          {`${errors.password && errors.password.message}`}
+        </FormErrorMessage>
 
         <Button
           w="10rem"
@@ -140,10 +157,32 @@ function Login() {
           fontWeight="normal"
           my="2rem"
           boxShadow="rgb(0 0 0 / 40%) 0px 3px 5px"
-          onClick={handleSubmit}
+          onClick={handleSubmit(submitLogin)}
         >
           {'Sign in'}
         </Button>
+        {isIncorrectLogs === true ? (
+          <Text
+            color="var(--chakra-colors-red-500)"
+            fontFamily="body"
+            fontSize="0.87rem"
+            mt="-1rem"
+            mb="1rem"
+          >
+            {'Username and / or password incorrect'}
+          </Text>
+        ) : null}
+        {isServerError === true ? (
+          <Text
+            color="var(--chakra-colors-red-500)"
+            fontFamily="body"
+            fontSize="0.87rem"
+            mt="-1rem"
+            mb="1rem"
+          >
+            {'There seems to be an error, try again in a few minutes.'}
+          </Text>
+        ) : null}
       </FormControl>
 
       <Divider borderColor="grey" width="75%" maxW="400px" />
