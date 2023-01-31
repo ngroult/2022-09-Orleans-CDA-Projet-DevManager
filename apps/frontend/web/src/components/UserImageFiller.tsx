f3856daa7f5703dacd57fcdea0f98d82d47c232d
codelande <co.delande@yahoo.fr>
2023-01-31 10:21:46 +0100

    feat: created utils folder and fetch images function

--- apps/frontend/web/src/components/UserImageFiller.tsx
+++ apps/frontend/web/src/components/UserImageFiller.tsx
@@ -1,4 +1,5 @@
 import { Box, Image, Grid, useRadioGroup, useRadio } from '@chakra-ui/react';
+import { useEffect } from 'react';
 
 const UserImageFiller = ({
   selectedImage,
@@ -9,6 +10,8 @@ const UserImageFiller = ({
 }) => {
   const { getRootProps, getRadioProps } = useRadioGroup();
   const group = getRootProps();
+
+  useEffect;
 
   const options = [
     'man1',
