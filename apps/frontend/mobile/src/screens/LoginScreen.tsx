4d5590c7093fb5c262c181feb70b8c65fa4e16a1
Paul Chatelain-Fernandes <paulchatelainfernandes@gmail.com>
2023-02-01 11:27:28 +0100

    feat(frontend/mobile): try to make login logic

--- apps/frontend/mobile/src/screens/LoginScreen.tsx
+++ apps/frontend/mobile/src/screens/LoginScreen.tsx
@@ -1,15 +1,11 @@
 import { useState } from 'react';
 import {
   Text,
-  Box,
-  Center,
   Heading,
-  VStack,
   FormControl,
   Input,
   Link,
   Button,
-  HStack,
   Flex,
   Divider,
 } from 'native-base';
@@ -18,6 +14,51 @@ export default function LoginScreen({ navigation }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isVisiblePassword, setIsVisiblePassword] = useState(false);
+  const [errors, setErrors] = useState<{
+    username?: { message: string };
+    password?: { message: string };
+  } | null>(null);
+  const [serverError, setServerError] = useState<string | null>(null);
+
+  const submitLogin = async () => {
+    if (errors?.username !== undefined) {
+      setErrors({
+        ...errors,
+        username: { message: 'Your username is required' },
+      });
+    }
+    if (errors?.password !== undefined) {
+      setErrors({
+        ...errors,
+        password: { message: 'Your password is required' },
+      });
+    }
+
+    if (errors === null) {
+      try {
+        const loginResponse = await fetch('/api/auth/login', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        });
+        const jsonResponse = await loginResponse.json();
+
+        if (jsonResponse.status === 'KO') {
+          setServerError('Username and/or password is incorrect');
+        }
+        if (jsonResponse.status === 'OK') {
+          navigation.navigate('Overview');
+        }
+      } catch (err) {
+        setServerError(
+          'There seems to be an error, try again in a few minutes.'
+        );
+      }
+    }
+  };
 
   return (
     <Flex flex="1" backgroundColor="turquoise.200">
@@ -36,7 +77,11 @@ export default function LoginScreen({ navigation }) {
         </Heading>
       </Flex>
       <Flex align="center" justify="center" px="12">
-        <FormControl isRequired pt="8">
+        <FormControl
+          isRequired
+          pt="8"
+          isInvalid={errors?.username ? true : false}
+        >
           <FormControl.Label>
             <Text fontSize="md" color="#000" mb="-2" ml="3">
               {'Username'}
@@ -52,7 +97,12 @@ export default function LoginScreen({ navigation }) {
             onChange={(e) => setUsername(e.target.value)}
           />
         </FormControl>
-        <FormControl isRequired mt="3">
+
+        <FormControl
+          isRequired
+          mt="3"
+          isInvalid={errors?.password ? true : false}
+        >
           <FormControl.Label>
             <Text fontSize="md" color="#000" mb="-2" ml="3">
               {'Password'}
@@ -69,12 +119,13 @@ export default function LoginScreen({ navigation }) {
             value={password}
           />
         </FormControl>
+
         <Button
           mt="5"
           w="150"
           borderRadius="8"
           backgroundColor="turquoise.900"
-          onPress={() => navigation.navigate('Overview')}
+          onPress={() => submitLogin()}
         >
           {'Sign in'}
         </Button>
