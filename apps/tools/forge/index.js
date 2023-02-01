c94cafb621c4a90616a147187b5c2f21e63d5245
Paul Chatelain-Fernandes <paulchatelainfernandes@gmail.com>
2023-02-01 17:15:39 +0100

    feat: add new project for launch forge

--- apps/tools/forge/index.js
+++ apps/tools/forge/index.js
@@ -1,14 +1,14 @@
-const launch = async () => {
-  const chalk = (await import('chalk')).default;
+import chalk from 'chalk';
 
+const launch = async () => {
   try {
-    const response = await fetch('http://localhost:3333/api/forge', {
+    const response = await fetch('http://localhost/api/forge', {
       method: 'POST',
     });
     if (response.status === 201) {
       console.log(chalk.green('Success! Fake data inserted into database.'));
     } else {
-      console.log(chalk.red('Error! Try again please.'));
+      console.log(chalk.red('Error! Is the server launched?'));
     }
   } catch (err) {
     console.log(chalk.red('Error! Is the server launched?'));
