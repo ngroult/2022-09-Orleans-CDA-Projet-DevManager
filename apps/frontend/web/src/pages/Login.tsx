a90a953692d90c4e33c4933ab30b75253e11aeea
Paul Chatelain-Fernandes <paulchatelainfernandes@gmail.com>
2023-02-02 12:25:04 +0100

    feat(frontend/web): handle press enter to submit form

--- apps/frontend/web/src/pages/Login.tsx
+++ apps/frontend/web/src/pages/Login.tsx
@@ -83,105 +83,107 @@ function Login() {
         </Text>
       </Flex>
 
-      <FormControl
-        display="flex"
-        flexDir="column"
-        alignItems="center"
-        justifyContent="center"
-        p="2rem 2rem 0"
-        isInvalid={errors.username || errors.password ? true : false}
-      >
-        <FormLabel textAlign="left" m="1rem 0 0" w="100%" maxW="400px">
-          {'Username'}
-        </FormLabel>
-        <Input
-          maxW="400px"
-          placeholder="Enter username..."
-          bgColor="#fff"
-          _placeholder={{ opacity: 0.3 }}
-          type="text"
-          {...register('username', {
-            required: 'Your username is required',
-            value: username,
-            onChange: (e) => setUsername(e.target.value),
-          })}
-        />
-        {errors.username ? (
-          <FormErrorMessage
-            m="0 0 -1rem"
-            h="1.8rem"
-          >{`${errors.username.message}`}</FormErrorMessage>
-        ) : (
-          <Box mb="-1rem" h="1.8rem"></Box>
-        )}
-
-        <FormLabel textAlign="left" m="1rem 0 0" w="100%" maxW="400px">
-          {'Password'}
-        </FormLabel>
-        <InputGroup size="md" maxW="400px">
+      <Box as="form" onSubmit={handleSubmit(submitLogin)} w="100%">
+        <FormControl
+          display="flex"
+          flexDir="column"
+          alignItems="center"
+          justifyContent="center"
+          p="2rem 2rem 0"
+          isInvalid={errors.username || errors.password ? true : false}
+        >
+          <FormLabel textAlign="left" m="1rem 0 0" w="100%" maxW="400px">
+            {'Username'}
+          </FormLabel>
           <Input
             maxW="400px"
-            placeholder="Enter password..."
+            placeholder="Enter username..."
             bgColor="#fff"
             _placeholder={{ opacity: 0.3 }}
-            type={isVisiblePassword ? 'text' : 'password'}
-            {...register('password', {
-              required: 'Your password is required',
-              value: password,
-              onChange: (e) => setPassword(e.target.value),
+            type="text"
+            {...register('username', {
+              required: 'Your username is required',
+              value: username,
+              onChange: (e) => setUsername(e.target.value),
             })}
           />
-          <InputRightElement width="4.5rem">
-            <Button
-              h="1.75rem"
-              size="sm"
-              me="2"
-              onClick={() => setIsVisiblePassword(!isVisiblePassword)}
-            >
-              {isVisiblePassword ? (
-                <div>
-                  {'Hide'} <ViewOffIcon ms="0.5" />
-                </div>
-              ) : (
-                <div>
-                  {'Show'} <ViewIcon ms="0.5" />
-                </div>
-              )}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-        {errors.password ? (
-          <FormErrorMessage
-            m="0 0 -1rem"
-            h="1.8rem"
-          >{`${errors.password.message}`}</FormErrorMessage>
-        ) : (
-          <Box mb="-1rem" h="1.8rem"></Box>
-        )}
+          {errors.username ? (
+            <FormErrorMessage
+              m="0 0 -1rem"
+              h="1.8rem"
+            >{`${errors.username.message}`}</FormErrorMessage>
+          ) : (
+            <Box mb="-1rem" h="1.8rem"></Box>
+          )}
 
-        <Button
-          w="10rem"
-          bgColor="turquoise.900"
-          color="#FFF"
-          fontWeight="normal"
-          my="2rem"
-          boxShadow="rgb(0 0 0 / 40%) 0px 3px 5px"
-          onClick={handleSubmit(submitLogin)}
-        >
-          {'Sign in'}
-        </Button>
-        {serverError !== null ? (
-          <Text
-            color="var(--chakra-colors-red-500)"
-            fontFamily="body"
-            fontSize="0.87rem"
-            mt="-1rem"
-            mb="1rem"
+          <FormLabel textAlign="left" m="1rem 0 0" w="100%" maxW="400px">
+            {'Password'}
+          </FormLabel>
+          <InputGroup size="md" maxW="400px">
+            <Input
+              maxW="400px"
+              placeholder="Enter password..."
+              bgColor="#fff"
+              _placeholder={{ opacity: 0.3 }}
+              type={isVisiblePassword ? 'text' : 'password'}
+              {...register('password', {
+                required: 'Your password is required',
+                value: password,
+                onChange: (e) => setPassword(e.target.value),
+              })}
+            />
+            <InputRightElement width="4.5rem">
+              <Button
+                h="1.75rem"
+                size="sm"
+                me="2"
+                onClick={() => setIsVisiblePassword(!isVisiblePassword)}
+              >
+                {isVisiblePassword ? (
+                  <div>
+                    {'Hide'} <ViewOffIcon ms="0.5" />
+                  </div>
+                ) : (
+                  <div>
+                    {'Show'} <ViewIcon ms="0.5" />
+                  </div>
+                )}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          {errors.password ? (
+            <FormErrorMessage
+              m="0 0 -1rem"
+              h="1.8rem"
+            >{`${errors.password.message}`}</FormErrorMessage>
+          ) : (
+            <Box mb="-1rem" h="1.8rem"></Box>
+          )}
+
+          <Button
+            w="10rem"
+            bgColor="turquoise.900"
+            color="#FFF"
+            fontWeight="normal"
+            my="2rem"
+            boxShadow="rgb(0 0 0 / 40%) 0px 3px 5px"
+            type="submit"
           >
-            {serverError}
-          </Text>
-        ) : null}
-      </FormControl>
+            {'Sign in'}
+          </Button>
+          {serverError !== null ? (
+            <Text
+              color="var(--chakra-colors-red-500)"
+              fontFamily="body"
+              fontSize="0.87rem"
+              mt="-1rem"
+              mb="1rem"
+            >
+              {serverError}
+            </Text>
+          ) : null}
+        </FormControl>
+      </Box>
 
       <Divider borderColor="grey" width="75%" maxW="400px" />
 
